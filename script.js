54e3ee01272570fca048ce8261c686ef7bdd09c9
20David03 <davoadamyan64@gmail.com>
2021-03-25 13:31:25 +0400

    second commit

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
 window.addEventListener("load", ()=>{
     const sounds=document.querySelectorAll(".sound");
-    const pads=document.querySelectorAll(".pads div");
-
-
+    const pads=document.querySelectorAll(".pads .pad");
 
 
     pads.forEach((pad,index)=>{
-    pad.addEventListener("click", function(){
-        sounds[index].currentTime=0
-        sounds[index].play()
+    pad.addEventListener("click", ()=>{
+        if(sounds[index].paused){
+            sounds[index].currentTime=0
+            sounds[index].play()
+            pad.onmouseout = function() {
+                    pad.style.opacity=1
+
+            };
+        }
+        else{
+            sounds[index].pause()
+        }
+
+        sounds[index].onended=function(){
+            pad.style.opacity=0.2
+        }
+
     })
 
+    pad.onmouseover = function (){
+        pad.style.opacity=1
+        };
+    
+    pad.onmouseout = function() {
+        pad.style.opacity=0.2
+    };
+
+
+
 })
+
 });
